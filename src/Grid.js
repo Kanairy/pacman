100117914382153c83bf422557acc38fe58c76ab
Harry Aydin <harry.aydin.23@gmail.com>
2017-01-13 22:38:01 +1100

    grid coordinate validation implemented

--- src/Grid.js
+++ src/Grid.js
@@ -1,7 +1,19 @@
 export default class Grid {
-  constructor() {
+  place(x, y) {
+    if (this.coordinatesValid(x, y)) {
+      this.x = x;
+      this.y = y;
+      return true;
+    }
+    return false;
   }
-  place() {
-    return true;
+  getState() {
+    return {
+      x: this.x,
+      y: this.y
+    };
+  }
+  coordinatesValid(x, y) {
+    return (x >= 0 && x <= 5 && y >= 0 && y <= 5) ? true: false;
   }
 }
--- test/grid.spec.js
+++ test/grid.spec.js
@@ -6,10 +6,25 @@ describe('Grid', () => {
 
   describe('#place', () => {
     it('should have a place command', () => {
-      expect(grid.place()).to.be.truthy;
-    })
+      expect(grid.place()).to.not.be.undefined;
+    });
+
     it('should allow placement at valid coordinates', () => {
-      expect(grid.place(0,0)).to.be.truthy;
-    })
-  })
+      expect(grid.place(0,0)).to.be.true;
+    });
+
+    it('should not allow negative coordinates', () => {
+      expect(grid.place(-1,-1)).to.be.false;
+    });
+    
+
+    it('should not allow x coordinate above 5', () => {
+      expect(grid.place(6,1)).to.be.false;
+    });
+
+    it('should not allow y coordinate above 5', () => {
+      expect(grid.place(1,6)).to.be.false;
+    });
+
+  });
 });
